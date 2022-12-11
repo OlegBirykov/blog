84c24d77dd03237a99f53bca4e504a2749112035
OlegBirykov <p_petro75@mail.ru>
2022-12-11 17:24:05 +0300

    Уроки 5-7

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="/app.css">
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex orci, faucibus et dapibus sit amet, ultrices ut odio. Aenean vel auctor purus, vitae tristique justo. Suspendisse tempus pharetra pellentesque. Aliquam ante nisl, accumsan non interdum in, vehicula sit amet nibh. Duis sit amet neque felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed facilisis eget eros vitae efficitur. Duis sit amet nunc vehicula, hendrerit metus non, vestibulum risus. Praesent interdum velit vitae egestas ultricies. Sed accumsan diam nisi, ut rhoncus eros gravida vitae. Nam viverra ac augue a accumsan. Duis pharetra convallis orci, mollis ullamcorper est. Sed aliquam luctus leo, a scelerisque dui finibus sed.    
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex orci, faucibus et dapibus sit amet, ultrices ut odio. Aenean vel auctor purus, vitae tristique justo. Suspendisse tempus pharetra pellentesque. Aliquam ante nisl, accumsan non interdum in, vehicula sit amet nibh. Duis sit amet neque felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed facilisis eget eros vitae efficitur. Duis sit amet nunc vehicula, hendrerit metus non, vestibulum risus. Praesent interdum velit vitae egestas ultricies. Sed accumsan diam nisi, ut rhoncus eros gravida vitae. Nam viverra ac augue a accumsan. Duis pharetra convallis orci, mollis ullamcorper est. Sed aliquam luctus leo, a scelerisque dui finibus sed.    
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex orci, faucibus et dapibus sit amet, ultrices ut odio. Aenean vel auctor purus, vitae tristique justo. Suspendisse tempus pharetra pellentesque. Aliquam ante nisl, accumsan non interdum in, vehicula sit amet nibh. Duis sit amet neque felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed facilisis eget eros vitae efficitur. Duis sit amet nunc vehicula, hendrerit metus non, vestibulum risus. Praesent interdum velit vitae egestas ultricies. Sed accumsan diam nisi, ut rhoncus eros gravida vitae. Nam viverra ac augue a accumsan. Duis pharetra convallis orci, mollis ullamcorper est. Sed aliquam luctus leo, a scelerisque dui finibus sed.    
+        </p>
+    </article>
+</body>
