c63cdebbcb06dfd0462e3fd5b1a937e1b8db6cdc
OlegBirykov <p_petro75@mail.ru>
2022-12-13 23:25:20 +0300

    Урок 8

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,7 +5,7 @@
 
 <body>
     <article>
-        <h1><a href="/post">My First Post</a></h1>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex orci, faucibus et dapibus sit amet, ultrices ut odio. Aenean vel auctor purus, vitae tristique justo. Suspendisse tempus pharetra pellentesque. Aliquam ante nisl, accumsan non interdum in, vehicula sit amet nibh. Duis sit amet neque felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed facilisis eget eros vitae efficitur. Duis sit amet nunc vehicula, hendrerit metus non, vestibulum risus. Praesent interdum velit vitae egestas ultricies. Sed accumsan diam nisi, ut rhoncus eros gravida vitae. Nam viverra ac augue a accumsan. Duis pharetra convallis orci, mollis ullamcorper est. Sed aliquam luctus leo, a scelerisque dui finibus sed.    
@@ -13,7 +13,7 @@
     </article>
 
     <article>
-        <h1><a href="/post">My Second Post</a></h1>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex orci, faucibus et dapibus sit amet, ultrices ut odio. Aenean vel auctor purus, vitae tristique justo. Suspendisse tempus pharetra pellentesque. Aliquam ante nisl, accumsan non interdum in, vehicula sit amet nibh. Duis sit amet neque felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed facilisis eget eros vitae efficitur. Duis sit amet nunc vehicula, hendrerit metus non, vestibulum risus. Praesent interdum velit vitae egestas ultricies. Sed accumsan diam nisi, ut rhoncus eros gravida vitae. Nam viverra ac augue a accumsan. Duis pharetra convallis orci, mollis ullamcorper est. Sed aliquam luctus leo, a scelerisque dui finibus sed.    
@@ -21,7 +21,7 @@
     </article>
 
     <article>
-        <h1><a href="/post">My Third Post</a></h1>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
 
         <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ex orci, faucibus et dapibus sit amet, ultrices ut odio. Aenean vel auctor purus, vitae tristique justo. Suspendisse tempus pharetra pellentesque. Aliquam ante nisl, accumsan non interdum in, vehicula sit amet nibh. Duis sit amet neque felis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed facilisis eget eros vitae efficitur. Duis sit amet nunc vehicula, hendrerit metus non, vestibulum risus. Praesent interdum velit vitae egestas ultricies. Sed accumsan diam nisi, ut rhoncus eros gravida vitae. Nam viverra ac augue a accumsan. Duis pharetra convallis orci, mollis ullamcorper est. Sed aliquam luctus leo, a scelerisque dui finibus sed.    
