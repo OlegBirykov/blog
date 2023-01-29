893eea5fd922d3090a34ae761f0bc30a3e247939
olegbirukov <p_petro75@mail.ru>
2023-01-29 17:56:43 +0300

    Урок 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,43 +17,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        Category::truncate();
-        Post::truncate();
-
-        $user = User::factory()->create();
-
-        $personal = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal',
+        $user = User::factory()->create([
+            'name' => 'petro'
         ]);
 
-        $family = Category::create([
-            'name' => 'Family',
-            'slug' => 'family',
-        ]);
-
-        $work = Category::create([
-            'name' => 'Work',
-            'slug' => 'work',
-        ]);
-
-        Post::create([
-          'user_id' => $user->id,
-          'category_id' => $family->id,
-          'title' => 'My Family Post',
-          'slug' => 'my-family-post',
-          'excerpt' => '<p>Это пост о чём-то</p>',
-          'body' => '<p>С точки зрения банальной эрудиции, мы не вправе игнорировать тенденции парадоксальных эмоций данного индивидуума, ибо его концепция при данных интерпретациях выглядит весьма логично</p>',
-        ]);
-
-        Post::create([
-          'user_id' => $user->id,
-          'category_id' => $work->id,
-          'title' => 'My Work Post',
-          'slug' => 'my-work-post',
-          'excerpt' => '<p>Это пост о чём-то</p>',
-          'body' => '<p>С точки зрения банальной эрудиции, мы не вправе игнорировать тенденции парадоксальных эмоций данного индивидуума, ибо его концепция при данных интерпретациях выглядит весьма логично</p>',
+        Post::factory(5)->create([
+            'user_id' => $user->id
         ]);
     }
 }
