e1bb4d705b251d2ab923157e0c7f2b5c364a3818
olegbirukov <p_petro75@mail.ru>
2023-02-18 22:29:08 +0300

    Урок 47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -19,8 +19,13 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name') }}"
                         required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
 
@@ -37,8 +42,13 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username') }}"
                         required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -54,8 +64,13 @@
                         type="text"
                         name="email"
                         id="email"
+                        value="{{ old('email') }}"
                         required
                     >
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -73,6 +88,10 @@
                         id="password"
                         required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
