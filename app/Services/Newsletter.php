1a6cb7c77b0acc1c664ef6864811b5c28920afc1
olegbirukov <p_petro75@mail.ru>
2023-07-30 19:41:17 +0300

    Урок 61

--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -6,12 +6,17 @@ use Illuminate\Support\Facades\Http;
 
 class Newsletter
 {
+    private $apiKey;
+
+    public function __construct() {
+        $this->apiKey = config('services.mailopost.key');
+    }
+
     public function subscribe(string $email, string $listId = null) {
-        $apiKey = config('services.mailopost.key');
         $listId ??= config('services.mailopost.lists.subscribers');
     
         $response = Http::withHeaders([
-            'Authorization' => 'Bearer ' . $apiKey, 
+            'Authorization' => 'Bearer ' . $this->apiKey, 
             'Content-Type' => 'application/json',
         ])->post('https://api.mailopost.ru/v1/email/lists/' . $listId . '/recipients', [
             'email' => $email,
