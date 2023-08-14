0e8d21c9cb8a3d437c71471ea0a79dedeceebdf2
olegbirukov <p_petro75@mail.ru>
2023-08-14 20:55:57 +0300

    Урок 69

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,6 +27,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-       Model::unguard();
+        Model::unguard();
+
+        Gate::define('admin', function (User $user) {
+            return $user->username === 'petro';
+        });
+
+        Blade::if('admin', function() {
+            return request()->user()?->can('admin');
+        });
     }
 }
